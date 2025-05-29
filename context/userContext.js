f2373cd4fce61d592ee5704c511b016500459126
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-05-29 22:40:33 +0530

    added navigations

--- context/userContext.js
+++ context/userContext.js
@@ -5,12 +5,15 @@ export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
+  const [activeTab, setActiveTab] = useState("");
 
   return (
     <UserContext.Provider
       value={{
         loading,
         setLoading,
+        activeTab,
+        setActiveTab,
       }}
     >
       {children}
