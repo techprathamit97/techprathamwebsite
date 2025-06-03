5cb76869fc0c72c3ad79a2d0946e282de41ec01f
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-06-03 22:56:27 +0530

    added new components

---
+++ src/index/components/CertificationHome/CertificationHome.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CertificationHome = () => {
+  return (
+    <div>CertificationHome</div>
+  )
+}
+
+export default CertificationHome
---
+++ src/index/components/EducatorHome/EducatorHome.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EducatorHome = () => {
+  return (
+    <div>EducatorHome</div>
+  )
+}
+
+export default EducatorHome
