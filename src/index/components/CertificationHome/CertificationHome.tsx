4e6026fe5dea0b69dc6c615190a395b514e20a66
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-06-04 22:22:13 +0530

    added certificate section

--- src/index/components/CertificationHome/CertificationHome.tsx
+++ src/index/components/CertificationHome/CertificationHome.tsx
@@ -1,8 +1,74 @@
-import React from 'react'
+import React from 'react';
+import './certification.css';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+
+// import required modules
+import { EffectCoverflow, Autoplay, Pagination, Navigation } from 'swiper/modules';
+import Image from 'next/image';
 
 const CertificationHome = () => {
   return (
-    <div>CertificationHome</div>
+    <div className='w-10/12 h-auto flex flex-col gap-10 overflow-hidden'>
+      <div className="text-3xl font-bold mb-4 text-gray-800">Certification Partners</div>
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Autoplay, Navigation]}
+        className="mySwiper swiperCertificate"
+      >
+        <SwiperSlide>
+          <Image src="https://swiperjs.com/demos/images/nature-1.jpg" alt='' width={300} height={300} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src="https://swiperjs.com/demos/images/nature-2.jpg" alt='' width={300} height={300} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src="https://swiperjs.com/demos/images/nature-3.jpg" alt='' width={300} height={300} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src="https://swiperjs.com/demos/images/nature-4.jpg" alt='' width={300} height={300} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src="https://swiperjs.com/demos/images/nature-5.jpg" alt='' width={300} height={300} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src="https://swiperjs.com/demos/images/nature-6.jpg" alt='' width={300} height={300} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src="https://swiperjs.com/demos/images/nature-7.jpg" alt='' width={300} height={300} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src="https://swiperjs.com/demos/images/nature-8.jpg" alt='' width={300} height={300} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src="https://swiperjs.com/demos/images/nature-9.jpg" alt='' width={300} height={300} />
+        </SwiperSlide>
+      </Swiper>
+    </div>
   )
 }
 
