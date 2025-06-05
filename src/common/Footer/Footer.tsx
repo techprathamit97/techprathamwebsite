8e3ca81768d75a29d391efc5cd0984b87f50c9a2
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-06-05 15:49:31 +0530

    update navigations

--- src/common/Footer/Footer.tsx
+++ src/common/Footer/Footer.tsx
@@ -1,27 +1,119 @@
 import { Separator } from '@/components/ui/separator';
 import { FaLinkedin, FaYoutube, FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
     <div className='w-full h-auto flex items-center justify-center bg-[#1E2328] text-white'>
       <div className='md:w-10/12 w-11/12 flex flex-col py-10 gap-10'>
-        
+
         <div className='w-full flex flex-col'>
           <div className='text-xl font-semibold mb-1'>Follow Us!</div>
           <Separator className='mb-4 w-80' />
           <div className='flex flex-row gap-3'>
-            <FaFacebook className='w-8 h-8' />
-            <FaLinkedin className='w-8 h-8' />
-            <FaYoutube className='w-8 h-8' />
-            <FaInstagram className='w-8 h-8' />
-            <FaTwitter className='w-8 h-8' />
+            <div className='w-10 h-10 rounded-full bg-red-700 flex items-center justify-center'>
+              <FaFacebook className='w-6 h-6' />
+            </div>
+            <div className='w-10 h-10 rounded-full bg-red-700 flex items-center justify-center'>
+              <FaLinkedin className='w-6 h-6' />
+            </div>
+            <div className='w-10 h-10 rounded-full bg-red-700 flex items-center justify-center'>
+              <FaYoutube className='w-6 h-6' />
+            </div>
+            <div className='w-10 h-10 rounded-full bg-red-700 flex items-center justify-center'>
+              <FaInstagram className='w-6 h-6' />
+            </div>
+            <div className='w-10 h-10 rounded-full bg-red-700 flex items-center justify-center'>
+              <FaTwitter className='w-6 h-6' />
+            </div>
           </div>
         </div>
 
         <div className='w-full flex flex-col gap-2'>
-          <div className='text-xl font-semibold'>Techpratham</div>
-          <div>At Techpratham, we </div>
+          <Link href={'/'} aria-label='Techpratham'>
+            <Image src={'/navbar/techpratham.svg'} alt='' width={100} height={50} className='w-56 h-auto' />
+          </Link>
+          <div className='text-sm font-light'>At Tech Pratham, we are a forward thinking IT education dedicated to empowering individuals with the skills they need to thrive in the digital age. Our mission is to bridge the gap between traditional learning and the evolving demands of the tech industry by offering innovative, industry relevant training programs. What sets us apart is our commitment to creating a learning environment that is not only accessible but also engaging and impactful. With flexible schedules, expert mentorship, and real-world project experience, we ensure every student is equipped to achieve their career goals.</div>
+        </div>
+
+        <div className='w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
+          <div className='flex flex-col gap-1'>
+            <div className='text-base uppercase font-normal'>CIN:</div>
+            <div className='text-sm font-light'>U62013UP2025PTC223378</div>
+          </div>
+          <div className='flex flex-col gap-1'>
+            <div className='text-base uppercase font-normal'>GST:</div>
+            <div className='text-sm font-light'>09AALCT8794N1Z2</div>
+          </div>
+          <div className='flex flex-col gap-1'>
+            <div className='text-base uppercase font-normal'>Registered Office:</div>
+            <div className='text-sm font-light'>G-31, 1st Floor Sector-3, Noida 201301</div>
+          </div>
+          <div className='flex flex-col gap-1'>
+            <div className='text-base uppercase font-normal'>Noida Office:</div>
+            <div className='text-sm font-light'>C-2, Sector-1, Noida, Uttar Pradesh - 201301</div>
+          </div>
+          <div className='flex flex-col gap-1'>
+            <div className='text-base uppercase font-normal'>Hyderabad Office:</div>
+            <div className='text-sm font-light'>LVS Arcade, 71, Hitech, Madhapur Road, Jubilee Enclave, HITEC City, Hyderabad</div>
+          </div>
+        </div>
+
+        <div className='w-full grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6'>
+          <div className='flex flex-col gap-1'>
+            <div className='text-base uppercase font-normal'>Certification Courses</div>
+            <div className='flex flex-col capitalize text-sm font-light gap-1'>
+              <div>SAP TRAINING INSTITUTE IN INDIA</div>
+              <div>Workday Certification Course</div>
+              <div>Cloud Computing Certification Course</div>
+              <div>Microsoft Power BI Training course</div>
+              <div>Data Science Certification Course</div>
+              <div>JAVA Certification Course</div>
+              <div>Sp3D Mechanical Course</div>
+              <div>Generative AI Training</div>
+            </div>
+          </div>
+          <div className='flex flex-col gap-1'>
+            <div className='text-base uppercase font-normal'>Trending Certification Courses</div>
+            <div className='flex flex-col capitalize text-sm font-light gap-1'>
+              <div>Agile Testing Training</div>
+              <div>Appium Certification Training</div>
+              <div>AWS Certification Training</div>
+              <div>JMeter certification Training</div>
+              <div>PROFESSIONAL PROGRAMS</div>
+              <div>Online IT Courses India - SAP FICO Training at Tech Pratham</div>
+              <div>ServiceNow Certification Training</div>
+              <div>Software Testing using Selenium Training</div>
+            </div>
+          </div>
+          <div className='flex flex-col gap-1'>
+            <div className='text-base uppercase font-normal'>Company</div>
+            <div className='flex flex-col capitalize text-sm font-light gap-1'>
+              <div>About Us</div>
+              <div>Courses</div>
+              <div>Contact Us</div>
+              <div>Careers</div>
+              <div>Corporate Training</div>
+              <div>FAQ's</div>
+              <div>Privacy Policy</div>
+              <div>Terms & Conditions</div>
+              <div>Refund/Cancellation Policy</div>
+            </div>
+          </div>
+          <div className='flex flex-col gap-1'>
+            <div className='text-base uppercase font-normal'>Student Zone</div>
+            <div className='flex flex-col capitalize text-sm font-light gap-1'>
+              <div>Job Openings</div>
+              <div>Training Certificate</div>
+              <div>Reviews</div>
+              <div>Admission</div>
+              <div>Placement</div>
+              <div>Payment</div>
+              <div>24/7 Technical Support</div>
+            </div>
+          </div>
         </div>
 
       </div>
--- src/common/Navbar/Navbar.tsx
+++ src/common/Navbar/Navbar.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Input } from '@/components/ui/input';
 import { Component1Icon } from '@radix-ui/react-icons';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar = () => {
   return (
     <div className='w-full flex flex-col items-center justify-center shadowBorder'>
       <div className='bg-[#080E3A] text-white w-full h-auto flex items-center justify-center'>
         <div className='md:w-10/12 w-11/12 py-3 flex flex-row gap-6 justify-start items-center'>
-          <div>TechPratham</div>
-          <div className='flex flex-row gap-2 items-center justify-center pl-4'>
+          <Link href={'/'} aria-label='Techpratham'>
+            <Image src={'/navbar/techpratham.svg'} alt='' width={100} height={50} className='w-40 h-auto' />
+          </Link>
+          <div className='flex flex-row gap-1 items-center justify-center ml-4 cursor-pointer'>
             <Component1Icon className='rotate-45' />
             <span>Courses</span>
           </div>
           <Input className='max-w-96 bg-white text-black' />
-          <div className='flex flex-row gap-2 items-center justify-center'>
+          <div className='flex flex-row gap-1 items-center justify-center cursor-pointer'>
             <Component1Icon className='rotate-45' />
             <span>Corporate Training</span>
           </div>
@@ -21,16 +25,16 @@ const Navbar = () => {
       </div>
       <div className='w-full h-auto py-2 flex items-center justify-center'>
         <div className='md:w-10/12 w-11/12 py-3 text-sm flex flex-row gap-6 items-center justify-center'>
-          <div>About Us</div>
-          <div>Training Certificate</div>
-          <div>Job Openings</div>
-          <div>Admission</div>
-          <div>Reviews</div>
-          <div>Blogs</div>
-          <div>Payment</div>
-          <div>Contact Us</div>
-          <div>24/7 Technical Support</div>
-          <div>Student Zone</div>
+          <div className='cursor-pointer'>About Us</div>
+          <div className='cursor-pointer'>Training Certificate</div>
+          <div className='cursor-pointer'>Job Openings</div>
+          <div className='cursor-pointer'>Admission</div>
+          <div className='cursor-pointer'>Reviews</div>
+          <div className='cursor-pointer'>Blogs</div>
+          <div className='cursor-pointer'>Payment</div>
+          <div className='cursor-pointer'>Contact Us</div>
+          <div className='cursor-pointer'>24/7 Technical Support</div>
+          <div className='cursor-pointer'>Student Zone</div>
         </div>
       </div>
     </div>
