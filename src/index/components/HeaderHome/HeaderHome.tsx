73b6727b51c72d6272d42de7b538e00110a28ae6
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-06-05 15:52:52 +0530

    update recognition section

--- src/index/components/HeaderHome/HeaderHome.tsx
+++ src/index/components/HeaderHome/HeaderHome.tsx
@@ -13,20 +13,39 @@ import 'swiper/css/pagination';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import { certificate } from '@/components/assets/certificate';
 import Image from 'next/image';
+import { Card } from '@/components/ui/card';
 
 const HeaderHome = () => {
     return (
-        <div className='w-full h-auto flex flex-col items-center justify-center bg-gradient-to-r from-[#371414] to-[#D1090F] text-white'>
-            <div className='w-10/12 h-auto flex md:flex-row flex-col py-10'>
-                <div className='w-full flex flex-col'>
-                    <div className='text-2xl font-bold mt-4'>India's Leading Job Oriented Project Based Upskilling Platform.</div>
-                    <div>
-                        <h2>Our Mission</h2>
+        <div className='w-full h-auto flex flex-col items-center justify-center bg-gradient-to-r from-[#371414] to-[#D1090F] text-white overflow-hidden'>
+            <div className='w-10/12 h-auto flex md:flex-row flex-col items-start gap-20 pt-16 pb-24'>
+                <div className='w-8/12 flex flex-col gap-10'>
+                    <div className='text-3xl font-bold'>India's <span className='text-[#f8da52]'>Leading Job oriented Project</span> based Upskilling platform.</div>
+                    <div className='flex flex-col items-start'>
+                        <h2 className='text-xl font-bold text-[#f8da52]'>Our Mission</h2>
                         <p>1 Skill = 1 Job = 1 Family = 6 Person = Happy Life</p>
                         <p>Gain hands-on experience with real projects guided by industry experts</p>
                     </div>
+                    <section className='text-white overflow-x-hidden h-auto w-full'>
+                        <div className='flex nowrap whitespace-nowrap'>
+                            <section className="message">
+                                {certificate.map((item, index) => (
+                                    <Card key={index}>
+                                        <Image src={item.image} alt={item.altText} width={1440} height={500} className='w-44 h-full object-cover' />
+                                    </Card>
+                                ))}
+                            </section>
+                            <section className="message mx-4">
+                                {certificate.map((item, index) => (
+                                    <Card key={index}>
+                                        <Image src={item.image} alt={item.altText} width={1440} height={500} className='w-44 h-full object-cover' />
+                                    </Card>
+                                ))}
+                            </section>
+                        </div>
+                    </section>
                 </div>
-                <div className='flex flex-col items-center justify-center w-96 h-auto'>
+                <div className='flex flex-col items-center justify-center min-w-96 h-auto'>
                     <Swiper
                         autoplay={{
                             delay: 2500,
--- src/index/components/RecognitionHome/RecognitionHome.tsx
+++ src/index/components/RecognitionHome/RecognitionHome.tsx
@@ -1,8 +1,51 @@
+import { Button } from '@/components/ui/button'
+import { Dot } from 'lucide-react'
+import Image from 'next/image'
 import React from 'react'
 
 const RecognitionHome = () => {
   return (
-    <div>RecognitionHome</div>
+    <div className='w-full h-auto flex flex-col items-center justify-center py-20 gap-10 bg-black text-white'>
+      <div className='w-full h-auto flex flex-col text-center font-semibold uppercase text-2xl'>
+        <div className="text-red-600">Industry Recognition</div>
+        <div>Elevate your career with prestigious credentials</div>
+      </div>
+      <div className='w-10/12 h-auto flex flex-col items-center justify-center'>
+        <div className='flex flex-row gap-4 mb-8'>
+          <Button variant='outline'>Certifications</Button>
+          <Button variant='outline'>Affliations</Button>
+        </div>
+        <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+          <div className='flex flex-col gap-8 border border-white p-4 rounded-md'>
+            <div className='w-full flex flex-row items-center uppercase gap-2'>
+              <div className='w-14 h-14 flex items-center justify-center border border-white shadow rounded'>ISO</div>
+              <div className='text-2xl font-semibold'>ISO Certification</div>
+            </div>
+            <div>
+              <Image src='/home/certificate.png' alt='certificate' width={1440} height={500} className='w-full h-full object-cover' />
+            </div>
+          </div>
+          <div className='flex flex-col gap-8 border border-white p-4 rounded-md'>
+            <div className='w-full flex flex-row items-center uppercase gap-2'>
+              <div className='w-14 h-14 flex items-center justify-center border border-white shadow rounded'>ISO</div>
+              <div className='text-2xl font-semibold'>ISO Certification</div>
+            </div>
+            <div>
+              <Image src='/home/certificate.png' alt='certificate' width={1440} height={500} className='w-full h-full object-cover' />
+            </div>
+          </div>
+          <div className='flex flex-col gap-8 border border-white p-4 rounded-md'>
+            <div className='w-full flex flex-row items-center uppercase gap-2'>
+              <div className='w-14 h-14 flex items-center justify-center border border-white shadow rounded'>ISO</div>
+              <div className='text-2xl font-semibold'>ISO Certification</div>
+            </div>
+            <div>
+              <Image src='/home/certificate.png' alt='certificate' width={1440} height={500} className='w-full h-full object-cover' />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
