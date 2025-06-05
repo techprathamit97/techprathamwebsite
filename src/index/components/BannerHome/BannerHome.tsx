28b45f2c132023e091bcdbfe6841276bea8eb7cc
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-06-05 15:50:30 +0530

    update testimonial section

--- src/index/components/BannerHome/BannerHome.tsx
+++ src/index/components/BannerHome/BannerHome.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
 
 const BannerHome = () => {
   return (
-    <div>BannerHome</div>
+    <div className='w-full h-auto flex flex-col items-center justify-center py-20 gap-10 bg-black text-white'>
+      <div className='w-10/12 h-auto flex flex-row items-center justify-between'>
+        <div className='md:w-1/2 w-full flex flex-col gap-4 capitalize'>
+          <div>
+            <Badge>Formula for Growth</Badge>
+          </div>
+          <div className='text-3xl font-bold'>Why should you upskill now?</div>
+          <div>Join techpratham - indias premier training institute for a future-ready career</div>
+          <div>
+            <Button>Book Free Demo</Button>
+          </div>
+        </div>
+        <Image src='/home/certificate.png' alt='' width={400} height={400} className='md:w-1/2 w-full h-80 transition-all rounded border border-red-600' />
+      </div>
+    </div>
   )
 }
 
--- src/index/components/TestmonialHome/TestmonialHome.tsx
+++ src/index/components/TestmonialHome/TestmonialHome.tsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import React from 'react';
+import './testmonial.css';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { certificate } from '@/components/assets/certificate';
+import Image from 'next/image';
 
 const TestmonialHome = () => {
   return (
-    <div>TestmonialHome</div>
+    <div className='w-full h-auto flex flex-col items-center justify-center py-20 gap-10 bg-gradient-to-r from-[#371414] to-[#D1090F] text-white overflow-hidden'>
+      <div className='w-10/12 h-auto flex flex-col items-center text-center'>
+        <div className="text-2xl font-semibold capitalize">Our success lies in our learner's, success stories</div>
+        <div className='w-1/2'>Explore our student's experiences and discover how we've healped them achieve their goals through excellence and dedications.</div>
+      </div>
+      <div className='w-10/12 flex flex-col items-center justify-center h-auto'>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={20}
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+            },
+            768: {
+              slidesPerView: 2,
+            },
+            1024: {
+              slidesPerView: 3,
+            },
+            1280: {
+              slidesPerView: 4,
+            },
+          }}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay]}
+          className="mySwiper swiperClient"
+        >
+          {certificate.map((item, index) => (
+            <SwiperSlide key={index} className='shadow'>
+              <Image src={item.image} alt={item.altText} width={1440} height={500} className='w-80 h-full object-cover' />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
   )
 }
 
