08d5d1468e156f6cfb033e5f4fc35b58b3e0266c
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-06-05 19:52:22 +0530

    added new assets

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,20 @@ const IndexPage: NextPage = (props) => (
         <IndexController {...props}>
             <Head>
                 <link rel="icon" href="/favicon.ico" type="image/ico" sizes="70x70" />
-                <title>TechPratham</title>
+                <title>India's No.1 Best IT Training Institute in India | Corporate Learning</title>
+                <meta name="description" content="Corporate Learning is the best IT Training Institute in India for Industrial Training, provide training in 180+ courses as IT, Software, SAP, Data science & AWS." />
+                <meta name="keywords" content="India's No.1 IT Training Institute,IT training institute in delhi with placement, IT training institute near me, IT training institute in india, best IT training institute in delhi, IT training institute in ghaziabad, IT training institute in noida, IT training institute in gurgaon, Professional courses training online, Professional courses training near me, professional development training courses, IT training institute India" />
+                <meta name="author" content="the-bipu" />
+
+                <meta property="og:title" content="India's No.1 Best IT Training Institute in India | Corporate Learning" />
+                <meta property="og:description" content="Corporate Learning is the best IT Training Institute in India for Industrial Training, provide training in 180+ courses as IT, Software, SAP, Data science & AWS." />
+                <meta property="og:image" content="/navbar/techpratham.svg" />
+                <meta property="og:url" content="https://www.techpratham.com/" />
+
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content="India's No.1 Best IT Training Institute in India | Corporate Learning" />
+                <meta name="twitter:description" content="Corporate Learning is the best IT Training Institute in India for Industrial Training, provide training in 180+ courses as IT, Software, SAP, Data science & AWS." />
+                <meta name="twitter:image" content="/navbar/techpratham.svg" />
             </Head>
 
             <IndexView />
