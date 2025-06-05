650340574918cae762211407a4683f01413a2370
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-06-05 19:51:38 +0530

    update assets
    update assets layouts and metadata

---
+++ components/assets/certification.ts
@@ -0,0 +1,26 @@
+export const certification = [
+    {
+        image: "/home/certification/java.png",
+        altText: "Java Logo",
+    },
+    {
+        image: "/home/certification/aws.png",
+        altText: "AWS Logo",
+    },
+    {
+        image: "/home/certification/oracle.png",
+        altText: "Oracle Logo",
+    },
+    {
+        image: "/home/certification/python.png",
+        altText: "Python Logo",
+    },
+    {
+        image: "/home/certification/salesforce.png",
+        altText: "Salesforce Logo",
+    },
+    {
+        image: "/home/certification/workday.png",
+        altText: "Workday Logo",
+    },
+]
