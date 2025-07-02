1b8abb79888ad86bf250ed7781e891e50e439334
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-07-02 23:27:02 +0530

    added new components

---
+++ components/common/Session/Session.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Session = () => {
+    return (
+        <div className='w-full h-screen flex items-center justify-center text-3xl font-semibold'>Session Already Exists.</div>
+    )
+}
+
+export default Session
