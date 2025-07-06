616c48b6be07435f12b594e68deddc49c7f70f44
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-07-06 12:40:54 +0530

    Add new model and ShadCn components

--- models/course.js
+++ models/course.js
@@ -1,30 +1,33 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const curriculumSchema = new mongoose.Schema({
   que: { type: String, required: true },
-  ans: { type: String, required: true }
+  ans: { type: String, required: true },
 });
 
 const faqSchema = new mongoose.Schema({
   que: { type: String, required: true },
-  ans: { type: String, required: true }
+  ans: { type: String, required: true },
 });
 
-const courseSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  shortDesc: { type: String },
-  description: { type: String },
-  rating: { type: String },
-  duration: { type: String },
-  level: { type: String },
-  category: { type: String },
-  link: { type: String },
-  videoLink: { type: String },
-  curriculum_data: [curriculumSchema],
-  skills_data: [String],
-  faqs_data: [faqSchema]
-}, {
-  timestamps: true
-});
+const courseSchema = new mongoose.Schema(
+  {
+    link: { type: String, required: true, unique: true },
+    title: { type: String, required: true },
+    shortDesc: { type: String, required: true },
+    description: { type: String, required: true },
+    rating: { type: String, required: true },
+    duration: { type: String, required: true },
+    level: { type: String, required: true },
+    category: { type: String, required: true },
+    videoLink: { type: String, required: true },
+    curriculum_data: [curriculumSchema],
+    skills_data: [String],
+    faqs_data: [faqSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export default mongoose.models.Course || mongoose.model('Course', courseSchema);
+export default mongoose.models.Course || mongoose.model("Course", courseSchema);
