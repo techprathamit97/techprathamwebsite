0b5b4a43e38ebf37d13ee168f26f24c7d2961391
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-07-28 22:41:45 +0530

    update course & about page

--- src/about/components/HeaderAbout/HeaderAbout.tsx
+++ src/about/components/HeaderAbout/HeaderAbout.tsx
@@ -5,13 +5,7 @@ import './header.css';
 const HeaderAbout = () => {
     return (
         <div className='w-full h-auto flex flex-col items-center justify-center lg:pt-30 md:pt-24 sm:pt-24 pt-10'>
-            <div className='w-full h-auto text-white flex flex-col items-center justify-center headerImage'>
-                <div className='md:w-10/12 w-11/12 py-28 flex md:flex-row flex-col items-center justify-between'>
-                    <div className='w-1/2 text-left lg:text-3xl md:text-2xl sm:text-xl text-xl font-medium'>
-                        Our Mission is to build nation through education and beyond limitation.
-                    </div>
-                </div>
-            </div>
+            <Image src='/about/banner.png' alt='' width={1920} height={1080} className='w-full h-96 object-cover' />
             <div className='w-10/12 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-12 text-left text-base py-16'>
                 <div className='col-span-2 w-full h-auto flex items-center justify-center text-justify'>
                     At Tech Pratham, our Founders bring a collective experience of 35 years in the IT industry. As esteemed alumni of top institutions, their deep industry expertise and forward thinking vision drive our programs. They have worked with top MNCs, gaining invaluable insights into the industry's needs and future. We are a forward to IT education institute dedicated to empowering individuals with the skills they need to thrive in the digital age.
